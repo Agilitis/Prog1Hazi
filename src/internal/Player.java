93d13bb8f1376ca77ebc920d1275f62f270acb79
Qpica <cs.k.kenez@gmail.com>
2019-04-23 19:49:42 +0200

    Player osztaly kesz.

--- src/internal/Player.java
+++ src/internal/Player.java
@@ -2,15 +2,12 @@ package internal;
 
 
 import skeletontest.TestCaseStore;
-import utility.CommandInterpreter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * A jatekon belul a jatekos megtestesitoje. Ha a jatekos tesz valamit, ezen az osztalyon keresztul teszi.
@@ -24,12 +21,20 @@ class Player {
     void doAction() {
         try {BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             if(bufferedReader.ready()){
-                String command = bufferedReader.readLine();
+                bufferedReader.readLine();
                 System.out.println("The step options are:");
 
                 ArrayList<Field> stepOptions = orangutan.getStepOptions();
+                for(Field field : stepOptions){
+                    System.out.println(field.getName());
+                }
 
-
+                String sFieldToStep = bufferedReader.readLine();
+                for(Field field : stepOptions){
+                    if(field.getName().equals(sFieldToStep)){
+                        orangutan.move(field);
+                    }
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
